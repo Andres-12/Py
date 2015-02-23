de9b40d8882e87f39fe2f1bf61ed31feae07b2d2
root <root@localhost.localdomain>
2015-02-23 14:48:34 -0600

    Changes

--- python.py
+++ python.py
@@ -4,7 +4,9 @@ v="String"
 print v
 if v == "String":
     print "Entro al if"
-else:
-    print "No entro al if"
 if True:
     print "Es True"
+i = 1
+print "\n"
+while i <= 100:
+    print str(i) + ",\t"
